c3baf490559e49721db8ed98afec4c37fde27713
cjreads665 <shahid.alam57@gmail.com>
2024-11-03 18:08:59 +0530

    added name test case

--- cypress/e2e/SignUp.cy.js
+++ cypress/e2e/SignUp.cy.js
@@ -1,8 +1,14 @@
 import SignUp from "../PageObjects/SignUp"
 
 const signup = new SignUp();
-
+let invalidEmailFixture;
 describe('Testing of Sign Up registration page', () => {
+
+  before(()=>{
+    cy.fixture('invalidEmailSignup').then((userData)=>{
+      invalidEmailFixture = userData;
+    })
+  })
 
   beforeEach(()=>{
     cy.visit('/')
@@ -16,11 +22,19 @@ describe('Testing of Sign Up registration page', () => {
     signup.getConfirmPasswordErrorMessage().should('not.exist')
   })
 
+  it.only("Verify signing up using no data",()=>{
+    signup.clickSubmitButton()
+    signup.getEmailErrorMessage().should('be.visible')
+    signup.getPasswordErrorMessage().should('be.visible')
+    signup.getFirstNameErrorMessage().should('be.visible')
+    signup.getLastNameErrorMessage().should('be.visible')
+  })
+
   //this test case will FAIL since the website does not have any validation for it
   // please refer to the test cases file fgor further info
   it.skip("Verify signing up using invalid name",()=>{
     cy.fixture('invalidNameData').then((userData)=>{
-      userData.forEach(({email,firstName,lastName,password})=>{
+      userData.forEach(({firstName,lastName})=>{
         signup.enterFirstName(firstName)
         signup.enterLastName(lastName)
         signup.getFirstNameErrorMessage().should('be.visible')
@@ -29,7 +43,16 @@ describe('Testing of Sign Up registration page', () => {
     })
   })
 
-  it.only("Verify signing up using invalid email",()=>{
+  it("Verify signing up using invalid email",()=>{
+    invalidEmailFixture.forEach(({firstName,lastName,email,password})=>{
+      signup.fillFormAndSubmit(firstName,lastName,email,password,password)
+      signup.getEmailErrorMessage().should('be.visible')
+      cy.reload()
+      signup.getEmailErrorMessage().should('not.exist')
+    })
+  })
+
+  it("Verify signing up with different passwords for confirm password",()=>{
     
   })
 
